e2e14d7ea74b403753f3d93de3b63ddb9e68fd52
Serolo65 <123469158+Serolo65@users.noreply.github.com>
2025-12-14 12:46:29 +0200

    Add files via upload

---
+++ Untitled22.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void name(string name);
+
+int main() {
+
+	/*int x, y;
+
+	cout<<"Enter a value to be given its square root \n"<<endl;
+	cin>>x;
+	cout <<"The squareroot of "<<x<<" is "<< sqrt(x);
+
+	name();**/
+
+    name("jay");
+
+	return 0;
+}
+void name(string name){
+
+    cout<<name<<endl;}
+
